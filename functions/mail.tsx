d01ea245bfc65d269ac7183a03aece8290379710
colbyfayock <fayock@gmail.com>
2022-12-28 21:43:22 -0500

    test

---
+++ functions/mail.tsx
@@ -0,0 +1,26 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.handler = async function (event, context) {
+  const body = JSON.parse(event.body);
+
+  const message = `
+    Name: ${body.name}rn
+    Email: ${body.email}rn
+    Message: ${body.message}
+  `;
+
+  await mail.send({
+    to: process.env.MAIL_TO,
+    from: 'hello@colbyfayock.com',
+    subject: `New Message from ${body.name}!`,
+    text: message,
+    html: message.replace(/rn/g, '<br>'),
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Success!' }),
+  };
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-
+import { SyntheticEvent } from 'react';
 import styles from './App.module.scss';
 import { Input } from './components/input/input';
 import { Label } from './components/label/label';
 import { FormRow } from './components/form-row/form-row';
 import { SubmitButton } from './components/submit-button/submit-button';
+import { RiSendPlaneFill } from 'react-icons/ri';
 
 function App() {
+    async function handleOnSubmit(e: SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const formData = {};
+
+        // @ts-ignore
+        Array.from(e.currentTarget.elements).forEach((field: HTMLInputElement) => {
+            if (!field.name) return;
+            // @ts-ignore
+            formData[field.name] = field.value;
+        });
+
+        console.log('formData', formData)
+
+        await fetch('/.netlify/functions/mail', {
+            method: 'POST',
+            body: JSON.stringify(formData)
+        });
+    }
     return (
         <div className={styles.App}>
             <h1 className={styles.title}>Contact Us</h1>
             <p className={styles.description}>Have a question? Fill out the form below and we&apos;ll get back to you as soon as we can.</p>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleOnSubmit}>
                 <FormRow>
                     <Label>Name</Label>
-                    <Input id={'name'} /></FormRow>
+                    <Input id={'name'} name={'name'} /></FormRow>
                 <FormRow>
                     <Label>Email</Label>
-                    <Input /></FormRow>
+                    <Input name={'email'} /></FormRow>
                 <FormRow>
                     <Label>Message</Label>
-                    <Input />
+                    <Input name={'message'} />
                 </FormRow>
                 <FormRow>
                     <SubmitButton>
+                        <RiSendPlaneFill />
                         Submit</SubmitButton></FormRow></form>
         </div>
     );
